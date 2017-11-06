e7c01a98184b5df663a8b1a6c912b5bbf195b30a
donaldma <donald.ma@hotmail.ca>
2017-11-05 22:40:29 -0800

    djnsakdjas

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
