a1ad7a7675e4b38c11f0fc2f4131b133197f0888
mgloriaa09 <mika_g_9@hotmail.com>
2018-01-15 22:12:14 -0800

    wtf

---
+++ api/db/migrations/20180115220439_fuckupfix1.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('course', function (table) {
+      table.dropUnique(['userId', 'name']);
+      table.dropUnique('userId');
+      table.dropUnique('name');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('course', function (table) {
+      table.unique(['userId', 'name']);
+      table.unique('userId');
+      table.unique('name');
+    })
+  ])
+};
+
